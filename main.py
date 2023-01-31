5ab8e40c15c394f324eeab8735243019609128c8
StepanTWL <h3xp3ctb@gmail.com>
2023-01-31 21:52:57 +0200

    31.01.2023 home

--- main.py
+++ main.py
@@ -5,8 +5,12 @@ from work_string import parse_string
 
 def on_click():
     s = form.ResistFormula.text()
+    if not s:
+        return
     s = parse_string(s)
-    form.Result.setText(f'Result   {format(float(s), ".6f")}R')
+    s = float(format(float(s), ".6f"))
+    s = '{0:,}'.format(s).replace(',', '.', str(s).count(','))
+    form.Result.setText(f'Result   {s}R')
 
 
 Form, Window = uic.loadUiType("window.ui")
