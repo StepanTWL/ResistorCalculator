a08c2a322165cd8c0226c8533ce72b3b76d8ef52
Stapan <h3xp3ctb@gmail.com>
2023-03-03 17:05:09 +0200

    03.03.2023 work

--- transfer_ui_py.bat
+++ transfer_ui_py.bat
@@ -1,3 +1,7 @@
+start f:\__MY_PROJECT__\Python\ResistorCalculator\venv\Scripts\pyuic5.exe -x window.ui -o window.py
+start f:\__MY_PROJECT__\Python\ResistorCalculator\venv\Scripts\pyuic5.exe -x about.ui -o about.py
+start f:\__MY_PROJECT__\Python\ResistorCalculator\venv\Scripts\pyrcc5.exe D:\MyProject\Python\ResistorCalculator\images\icon.qrc -o icon_rc.py
+
 start pyuic5 -x window.ui -o window.py
 start pyuic5 -x about.ui -o about.py
 start pyrcc5 D:\MyProject\Python\ResistorCalculator\images\icon.qrc -o icon_rc.py
