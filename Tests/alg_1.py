97b2eb0edd0ad41f5d33fdcd9f231e81105557ed
Stapan <h3xp3ctb@gmail.com>
2023-03-09 18:06:40 +0200

    09.03.2023 work

--- Tests/alg_1.py
+++ Tests/alg_1.py
@@ -1,7 +1,7 @@
 import codecs
 
 
-def create_table_mgep():
+def create_table_crc32_bzip2_mpeg():
     a = []
     for i in range(256):
         k = i << 24
@@ -11,16 +11,7 @@ def create_table_mgep():
     return a
 
 
-def create_table_jamcrc():
-    a = []
-    for i in range(256):
-        k = i
-        for _ in range(8):
-            k = (k >> 1) ^ 0xEDB88320 if k & 0x00000001 else k >> 1
-        a.append(k)
-    return a
-
-def create_table():#same jamcrc
+def create_table_crc32_base_jamcrc():
     a = []
     for i in range(256):
         k = i
@@ -31,7 +22,7 @@ def create_table():#same jamcrc
 
 
 def crc32_jamcrc(frame):
-    crc_table = create_table_jamcrc()
+    crc_table = create_table_crc32_base_jamcrc()
     crc = 0xffffffff
     for byte in frame:
         crc = (crc >> 8) ^ crc_table[(crc ^ byte) & 0xFF]
@@ -39,17 +30,25 @@ def crc32_jamcrc(frame):
 
 
 def crc32_mpeg(frame):
-    crc_table = create_table_mgep()
+    crc_table = create_table_crc32_bzip2_mpeg()
     crc = 0xffffffff
     for byte in frame:
         crc = (crc << 8) ^ crc_table[((crc >> 24) ^ byte) & 0xFF]
     return crc & 0xFFFFFFFF
 
+
 def crc32(frame):
-    crc_table = create_table()
+    crc_table = create_table_crc32_base_jamcrc()
     crc = 0xffffffff
     for byte in frame:
         crc = (crc >> 8) ^ crc_table[(crc ^ byte) & 0xFF]
+    return crc ^ 0xFFFFFFFF
+
+def crc32_bzip2(frame):
+    crc_table = create_table_crc32_bzip2_mpeg()
+    crc = 0xffffffff
+    for byte in frame:
+        crc = (crc << 8) ^ crc_table[((crc >> 24) ^ byte) & 0xFF]
     return crc ^ 0xFFFFFFFF
 
 
@@ -57,14 +56,21 @@ def ascii_to_hex(s: str):
     s1 = bytes.fromhex(s).decode()
     return str.encode(s1)
 
-
-print(hex(crc32(ascii_to_hex('0c 00 00 00 00 00 00 00'))))
-
+s1 = hex(crc32_bzip2(ascii_to_hex('0c 00 00 00 00 00 00 00')))
+s = str(hex(crc32_bzip2(ascii_to_hex('0c 00 00 00 00 00 00 00'))))[2:].zfill(8)
+chunks = [s[i - 1:i + 1] for i in range(7, 0, -2)]
+print(chunks)
 
 """
 CRC-32 0c 00 00 00 00 00 00 00 - 0x328DCA26 - 26 CA 8D 32
 CRC-32 0c - 0xDBB4A3A6 - A6 A3 B4 DB
 width=32 poly=0x04c11db7 init=0xffffffff refin=true refout=true xorout=0xFFFFFFFF check=0xCBF43926 residue=0x00000000 name="CRC-32"
+"""
+
+"""
+CRC-32/BZIP2 0c 00 00 00 00 00 00 00 - 0x2831F707 - 07 F7 31 28
+CRC-32 0c - 0x84FBDB2F - 2F DB FB 84
+width=32 poly=0x04c11db7 init=0xffffffff refin=false refout=false xorout=0xFFFFFFFF check=0xFC891918 residue=0x00000000 name="CRC-32/BZIP2"
 """
 
 """
@@ -78,8 +84,6 @@ CRC-32/JAMCRC 0c 00 00 00 00 00 00 00 - 0xCD7235d9 - D9 35 72 CD
 CRC-32/JAMCRC 0c - 0x244B5C59 - 59 5C 4B 24
 width=32 poly=0x04c11db7 init=0xffffffff refin=true refout=true xorout=0x00000000 check=0x340BC6D9 residue=0x00000000 name="CRC-32/JAMCRC"
 """
-
-
 
 """
 def crc32_mpeg(frame):
